19733237bdc39290e53c8c53daea79cbd07ce725
Kyle Carter <kcarter134@yahoo.com>
2023-03-11 12:05:08 -0500

    homepage styling && navbar alignment w/ placeholders

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,16 @@ import './Header.css'
 function Header() {
   return(
     <nav className="nav-area">
-      <div>
-        <h1>Dumbledore's Army</h1>
-      </div>
+        <div className="title-wrapper">
+            <h1>Dumbledore's Army</h1>
+        </div>
+
+        <div className="link-wrapper">
+            <div className="nav-link">Spells</div>
+            <div className="nav-link">Houses</div>
+            <div className="nav-link">Placeholder</div>
+            <div className="nav-link">Placeholder</div>
+        </div>
     </nav>
   )
 }
