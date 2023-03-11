5252fb5f70a1f7e01f9e7528715bea52407b282d
AndrewCerveny <andrewfcerveny0628@gmail.com>
2023-03-11 14:58:08 -0500

    Started a Form Component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Form(){
+ return(
+  <form>
+    <input
+    placeholder="search"
+    />
+    <input
+    type={"radio"}
+    />
+     <input
+    type={"radio"}
+    />
+     <input
+    type={"radio"}
+    />
+     <input
+    type={"radio"}
+    />
+    <button>Submit</button>
+  </form>
+ )
+}
+export default Form;
