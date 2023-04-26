8ba163f47a363ce0d4ba5426de836901d0ad572a
AndrewCerveny <andrewfcerveny0628@gmail.com>
2023-04-25 19:15:10 -0600

    Added a gif to a user's choice page needs styling

--- src/components/Pages/CommonRoom/CommonRoom.js
+++ src/components/Pages/CommonRoom/CommonRoom.js
@@ -1,13 +1,24 @@
 import React from "react";
+import pictures from '../../../images/photos.gif'
+import './CommonRoom.css'
 
 const CommonRoom = () => {
 
 
   return(
     <section className="common-area">
-      
-
-    </section>
+      <img src={pictures} alt='moving photos on a wall' className='moving-pics' />
+      <section className="welcome-user"> Welcome to Hogwarts <span> insert name here</span></section>
+      <section className="agenda"> Decide today's agenda </section>
+      <section className="users-choice">
+          <div className="class-wrapper">
+          <h2> Go to class </h2>
+          </div>
+          <div className="requirement-wrapper">
+            <h2> Go To Room of Requirement</h2>
+          </div>
+      </section>
+   </section>
 
   )
 }
