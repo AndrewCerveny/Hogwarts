13fe7a501080638677af9540eb78dae01d7b2e36
AndrewCerveny <andrewfcerveny0628@gmail.com>
2023-05-15 12:55:01 +0200

    Working on Room of requirement

--- src/components/RequirementForm/RequirementForm.js
+++ src/components/RequirementForm/RequirementForm.js
@@ -10,7 +10,7 @@ const [ description, setDescription] = useState('')
   <form className="req-form">
     <input placeholder="make a wish" name="wish" className="r-input" />
     <input placeholder='description' name='description'className="r-input"/>
-    <input type='submit' placeholder="submit"className="r-input"/>
+    <button className="sub">Submit</button>
   </form>
   )
 }
